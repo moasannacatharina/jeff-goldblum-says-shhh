cdfa36d1b29f1a7585ffff943a984b1dafcfdc2a
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2020-12-08 15:39:40 +0100

    buttons

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-// const container = document.querySelector(".container");
-// const boxes = container.querySelectorAll("div");
-
-// // const img =
-// //   "https://www.geekwiththat.com/wp-content/uploads/2017/06/1898130_285125985022594_7169533043432192446_n.png";
-
-// boxes.forEach((box) => {
-//   box.addEventListener("click", () => {
-//     const imgTag = `<img src="${img}" alt="Jeff Goldblum"/>`;
-//     // box.innerHTML = imgTag;
-
-//     if (box.innerHTML === "") {
-//       box.innerHTML = imgTag;
-//     } else {
-//       box.innerHTML = "";
-//     }
-//   });
-// });
-
 let x = 0;
 let y = 0;
 const main = document.querySelector("main");
@@ -46,6 +27,16 @@ borderImgs.forEach((img) => {
   });
 });
 
+const slideImg = document.querySelector(".slide");
+const imgUrl =
+  "https://www.geekwiththat.com/wp-content/uploads/2017/06/1898130_285125985022594_7169533043432192446_n.png";
+
+// const addUrl = () => {
+//   slideImg.src = imgUrl;
+// };
+
+// setTimeout(addUrl, 6000);
+
 // const h1 = document.querySelector(".reveal-text");
 
 // function changeText() {
@@ -55,3 +46,25 @@ borderImgs.forEach((img) => {
 // setTimeout(changeText, 3000);
 
 // console.log(h1);
+
+const footer = document.querySelector("footer");
+const choices = ["I will keep talking", "I just wanna sing", "I will shut up"];
+
+choices.forEach((choice) => {
+  template = `<button>${choice}</button>`;
+  footer.innerHTML += template;
+});
+
+const buttons = footer.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.textContent === "I will keep talking") {
+      window.alert("NO!");
+    } else if (button.textContent === "I just wanna sing") {
+      window.alert("Oh my god. Stop.");
+    } else {
+      window.alert("Finally! Good job!");
+    }
+  });
+});
