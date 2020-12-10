091f1ba8180f73f9051145911526ca68f5b34066
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2020-12-10 16:02:47 +0100

    added fonts, animation, new compressed images

--- app.js
+++ app.js
@@ -3,39 +3,8 @@ let y = 0;
 const main = document.querySelector("main");
 const box = document.querySelector("div");
 const img = document.querySelector(".click-image");
+const textBox = document.querySelector(".textbox");
 
-main.addEventListener("click", (e) => {
-  img.style.display = "block";
-
-  x = e.offsetX;
-  y = e.offsetY;
-
-  img.style.top = e.offsetY + "px";
-  img.style.left = e.offsetX + "px";
-  textBox.innerHTML = "";
-});
-
-const slideImg = document.querySelector(".slide");
-const imgUrl =
-  "https://www.geekwiththat.com/wp-content/uploads/2017/06/1898130_285125985022594_7169533043432192446_n.png";
-
-// const addUrl = () => {
-//   slideImg.src = imgUrl;
-// };
-
-// setTimeout(addUrl, 6000);
-
-// const h1 = document.querySelector(".reveal-text");
-
-// function changeText() {
-//   h1.textContent = "click anywhere!";
-// }
-
-// setTimeout(changeText, 3000);
-
-// console.log(h1);
-
-const footer = document.querySelector("footer");
 const choices = [
   "I will keep talking",
   "I just wanna sing",
@@ -48,17 +17,90 @@ choices.forEach((choice) => {
   btnContainer.innerHTML += template;
 });
 
-const buttons = document.querySelectorAll("button");
-const textBox = main.querySelector(".textbox");
+const pictureSet = document.querySelector("picture");
+const introText = document.querySelector(".intro");
+const options = document.querySelector(".options");
+const secretBox = document.querySelector(".secret");
+
+const changeView = () => {
+  options.style.display = "flex";
+  pictureSet.style.display = "none";
+  secretBox.style.display = "block";
+  // introText.style.display = "none";
+
+  main.addEventListener("mousemove", (e) => {
+    img.style.display = "block";
+
+    x = e.pageX;
+    y = e.pageY;
+
+    img.style.top = e.pageY + "px";
+    img.style.left = e.pageX + "px";
+  });
+};
+
+setTimeout(changeView, 7000);
+
+const buttons = document.querySelectorAll(".btn");
+
+const responses = [
+  "uh, no, please don't.",
+  "are you dumb?",
+  "finally! thanks.",
+];
+
+let textBoxOpen = false;
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
+    textBoxOpen = true;
+
+    textBox.style.display = "flex";
+    textBox.style.animation = "slide 1s linear 1";
+
     if (button.textContent === "I will keep talking") {
-      textBox.innerHTML = `<h1>No! Please don't.</h1>`;
+      textBox.innerHTML = `<h1> ${responses[0]}</h1>`;
     } else if (button.textContent === "I just wanna sing") {
-      textBox.innerHTML = `<h1>Oh my god. Stop.</h1>`;
-    } else {
-      textBox.innerHTML = `<h1>Finally! Thanks.</h1>`;
+      textBox.innerHTML = `<h1>${responses[1]}</h1>`;
+    }
+
+    if (button.textContent === "Fine! I'll shut up") {
+      textBox.innerHTML = `<h1>${responses[2]}</h1>`;
+      celebrate();
     }
   });
+});
+
+const body = document.querySelector("body");
+
+body.addEventListener("click", (e) => {
+  const targetIsButton = e.target.className.match("btn");
+
+  if (targetIsButton) {
+    textBoxOpen = true;
+    textBox.style.display = "flex";
+  } else {
+    textBoxOpen = false;
+    textBox.style.display = "none";
+  }
+});
+
+const hiddenbtn = document.querySelector(".hidden");
+const audio = hiddenbtn.querySelector("audio");
+
+const play = () => {
+  audio.play();
+};
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 700) {
+    document.body.style.backgroundColor = "#7ba5f1";
+    hiddenbtn.style.display = "block";
+  } else {
+    document.body.style.backgroundColor = "var(--bg-color";
+  }
+});
+
+hiddenbtn.addEventListener("click", () => {
+  play();
 });
